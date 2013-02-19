d051289221fe1cf388d05d0021dacb9e3574c1df
khalif lahbibi <khalif.lahbibi@etu.u-bordeaux1.fr>
2013-02-19 11:16:13 +0100

    correction Makefile

--- v1/opp.c
+++ v1/opp.c
@@ -2,75 +2,88 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-  
+
+int getopt (int argc, char * const argv[], const char *optstring);
+extern char *optarg;
+extern int optind, opterr, optopt;
+
 int main(int argc, char *argv[])
 {  
-    int optch;  
-    extern int opterr;  
+  int optch;  
+  extern int opterr;  
   
-    char format[] = "hvctruxfzdsm";  
+  char format[] = "hvctruxfzdsm";  
   
-    opterr = 1;  
+  opterr = 1;  
   
-    while ((optch = getopt(argc, argv, format)) != -1)  
+  while ((optch = getopt(argc, argv, format)) != -1)  
     switch (optch) {  
-        case 'h':  
-            printf ("Aide :\n");  
-            printf ("Utilisation : tar [OPTION...] [FICHIER]...\n");  
-            break;  
-        case 'v':  
-            printf ("Paramètre v recontré : mode verbose\n");
-            break;  
-        case 'c':  
-			printf ("Paramètre c recontré : créer une archive à partir d'une liste de fichiers (et de répertoires)\n");
-            break;  
-        case 't':  
-            printf ("Paramètre t recontré : lister les fichiers (et répertoires) contenus dans une archive\n");
-            break;  
-        case 'r':  
-            printf ("Paramètre r recontré : ajouter de nouveaux fichiers (ou repertoires)\n");
-            break;  
-        case 'u':  
-            printf ("Paramètre u recontré\n");
-            break;  
-        case 'x':  
-            printf ("Paramètre x recontré\n");
-            break;  
-        case 'f':  
-            printf ("Paramètre f recontré\n");
-            break;  
-        case 'z':  
-            printf ("Paramètre z recontré\n");
-            break;  
-        case 'd':  
-            printf ("Paramètre d recontré\n");
-            break;  
-        case 's':  
-            printf ("Paramètre s recontré\n");
-            break;  
-        case 'm':  
-            printf ("Paramètre m recontré\n");
-            break; 
+    case 'h':  
+      printf ("Aide :\n");  
+      printf ("Utilisation : tar [OPTION...] [FICHIER]...\n");  
+      break;  
+    case 'v':  
+      printf ("Paramètre v recontré : mode verbose\n");
+      break;  
+    case 'c':  
+      printf ("Paramètre c recontré : créer une archive à partir d'une liste de fichiers (et de répertoires)\n");
+      break;  
+    case 't':  
+      printf ("Paramètre t recontré : lister les fichiers (et répertoires) contenus dans une archive\n");
+      break;  
+    case 'r':  
+      printf ("Paramètre r recontré : ajouter de nouveaux fichiers (ou repertoires)\n");
+      break;  
+    case 'u':  
+      printf ("Paramètre u recontré\n");
+      break;  
+    case 'x':  
+      printf ("Paramètre x recontré\n");
+      break;  
+    case 'f':  
+      printf ("Paramètre f recontré\n");
+      break;  
+    case 'z':  
+      printf ("Paramètre z recontré\n");
+      break;  
+    case 'd':  
+      printf ("Paramètre d recontré\n");
+      break;  
+    case 's':  
+      printf ("Paramètre s recontré\n");
+      break;  
+    case 'm':  
+      printf ("Paramètre m recontré\n");
+      break; 
     }  
+  
+  //affichage des arguments
+  for (; optind < argc; ++optind)  
+    printf ("argv[%d] : %s\n", optind, argv[optind]);
+  
+  
+  FILE *fichier = NULL;
+  fichier = fopen(argv[1], "r"); // ouverture fichier mode "read"
+  if (fichier == NULL) {
+    printf("erreur ouverture fichier\n");} 
+  int fcl;
+  if (fclose(fichier) != 0) {
+    printf("erreur fermeture fichier\n");}
+  else {
+    printf("fermeture fichier ok\n");}
+return 0;  
+}
 
-	//affichage des arguments
-    for (; optind < argc; ++optind)  
-        printf ("argv[%d] : %s\n", optind, argv[optind]);
-    
-    
-    FILE *fichier = NULL;
-    fichier = fopen(argv[1], "r"); // ouverture fichier mode "read"
-    if (fichier == NULL) {printf("erreur ouverture fichier\n");}
-    
-    
-    
-    
-    
-    /*
+ 
+  
+  
+  
+  
+  /*
     else
     {
-		printf("ouverture fichier ok\n");
-		
+    printf("ouverture fichier ok\n");
+    
 		char phrase[100000];  
   
 		while (fgets(phrase, 10000, fichier) != NULL)
@@ -80,54 +93,48 @@ int main(int argc, char *argv[])
 		}
 		
 	*/	
-		
-		/*
-		char lu[100000];
-		int i;
-		i=0;
-
-		fread(lu, 1000000000000000, 1, fichier);
-		for (i=0; i<100000; i++)
-		{
-			printf("lecture :   %s\n", lu[i]);
-		}
-		*/
-		
-		int fcl;
-	    if (fclose(fichier) != 0) {printf("erreur fermeture fichier\n");}
-	    else {printf("fermeture fichier ok\n");}
-	}
-    
-        
-/*    
-	int fd;
-	fd = open(argv[1], O_RDONLY, 0);
-	if (fd == -1)
-	{
-		fprintf (stderr, "erreur d'ouverture fichier %s\n", argv[1]);
-		return 1;
-	}
-	else 
-	{
-		fprintf(stdout, "ouverture fichier ok\n");
-		
-		int nbOct;
-		char lecture;
-		nbOct = 1;
-
-		while (nbOct > 0)
-		{
-			nbOct = read(fd, &lecture, 1);
-			printf ("nbOct : %i\n", nbOct);
-			fprintf (stdout, "lecture : %i\n", lecture);
-		}
-		
-		int cd;
-		cd = close(fd); //fermeture du fichier par son descripteur
-		if (cd == -1) fprintf (stderr, "erreur de fermeture fichier %s\n", argv[1]);
-		else fprintf(stdout, "fermeture fichier ok\n");
-	}
-*/  
-    return 0;  
   
-}
+  /*
+    char lu[100000];
+    int i;
+    i=0;
+    
+    fread(lu, 1000000000000000, 1, fichier);
+    for (i=0; i<100000; i++)
+    {
+    printf("lecture :   %s\n", lu[i]);
+    }
+  */
+
+
+
+
+/*    
+      int fd;
+      fd = open(argv[1], O_RDONLY, 0);
+      if (fd == -1)
+      {
+      fprintf (stderr, "erreur d'ouverture fichier %s\n", argv[1]);
+      return 1;
+      }
+      else 
+      {
+      fprintf(stdout, "ouverture fichier ok\n");
+		
+      int nbOct;
+      char lecture;
+      nbOct = 1;
+      
+      while (nbOct > 0)
+      {
+      nbOct = read(fd, &lecture, 1);
+      printf ("nbOct : %i\n", nbOct);
+      fprintf (stdout, "lecture : %i\n", lecture);
+      }
+      
+      int cd;
+      cd = close(fd); //fermeture du fichier par son descripteur
+      if (cd == -1) fprintf (stderr, "erreur de fermeture fichier %s\n", argv[1]);
+      else fprintf(stdout, "fermeture fichier ok\n");
+      }
+*/  
